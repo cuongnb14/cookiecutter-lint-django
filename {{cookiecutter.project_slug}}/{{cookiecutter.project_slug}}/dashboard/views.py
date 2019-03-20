bb44ad48ab506a9e8439c4799548e531ae0d1f7d
Cuong Nguyen <cuongnb14@gmail.com>
2019-03-20 22:05:52 +0700

    add material template

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/dashboard/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/dashboard/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
     context = dict()
     if request.method == 'GET':
-        return render(request, 'base.html', context)
+        return render(request, 'index.html', context)
